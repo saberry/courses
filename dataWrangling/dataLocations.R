85d9314a6eab877fd86ed476ae8d0249f174f25f
saberry <seth.berry@nd.edu>
2018-03-02 15:38:14 -0500

    More updates.

---
+++ dataWrangling/dataLocations.R
@@ -0,0 +1,25 @@
+####################################
+### Data Prep For Data Wrangling ###
+####################################
+
+jsonBrief = "https://www3.nd.edu/~sberry5/data/optionsDataBrief.json"
+
+jsonWhole = "https://www3.nd.edu/~sberry5/data/optionsDataComplete.json"
+
+merge1 = "https://www3.nd.edu/~sberry5/data/merge1Company.dta"
+
+merge2Hoberg = "https://www3.nd.edu/~sberry5/data/merge2Hoberg.txt"
+
+merge3McDonald = "https://www3.nd.edu/~sberry5/data/merge3McDonald.csv"
+
+spssExample = "https://www3.nd.edu/~sberry5/data/spssExample.sav"
+
+stataExample = "https://www3.nd.edu/~sberry5/data/stataExample.dta"
+
+sasExample = "https://www3.nd.edu/~sberry5/data/wciklink_gvkey.sas7bdat"
+
+fuzzyJoinClients = "https://www3.nd.edu/~sberry5/data/fuzzyJoinClients.csv"
+
+fuzzyJoinCompustat = "https://www3.nd.edu/~sberry5/data/fuzzyJoinCompustat.csv"
+
+sdcTest = "https://www3.nd.edu/~sberry5/data/sdcTest.txt"

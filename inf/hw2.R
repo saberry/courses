3594fd8921a3a380363b58434d346af2d9059a73
saberry <seth.berry@nd.edu>
2018-04-12 08:50:40 -0400

    Updates.

--- inf/hw2.R
+++ inf/hw2.R
@@ -1,24 +1,26 @@
 library(dplyr)
 
-employment1 = data.frame(daysEmployed = rnorm(500, mean = 220, sd = 50), 
-                        salary = rnorm(500, mean = 75000, sd = 10000), 
-                        daysAbsent = rnorm(500, mean = 2, sd = 10), 
-                        department = sample(c("IT", "HR", "Marketing"), 
-                                            500, replace = TRUE, prob = c(.2, .5, .3)), 
-                        education = sample(1:3, 500, replace = TRUE))
+employment1 = data.frame(daysEmployed = rnorm(462, mean = 220, sd = 50), 
+                        salary = rnorm(462, mean = 75000, sd = 10000), 
+                        daysAbsent = rnorm(462, mean = 2, sd = 10), 
+                        department = sample(c("IT", "CS", "Marketing", "HR"), 
+                                            462, replace = TRUE, prob = c(.1, .5, .3, .1)), 
+                        education = sample(1:3, 462, replace = TRUE), 
+                        gender = sample(1:2, 462, replace = TRUE))
 
 employment1$daysAbsent[which(employment1$daysAbsent < 0)] = 0
 
 employment1$daysEmployed[which(employment1$daysEmployed > 365)] = 365
 
 employment = data.frame(daysEmployed = rnorm(1000, mean = 250, sd = 50), 
-                        department = sample(c("IT", "HR", "Marketing"), 
-                                            1000, replace = TRUE, prob = c(.05, .555, .355)), 
+                        department = sample(c("IT", "CS", "Marketing", "HR"), 
+                                            1000, replace = TRUE, prob = c(.05, .505, .305, .05)), 
                         education = rep(0, 1000))
 
 employment = employment %>% 
   mutate(salary = daysEmployed + 80000 + rnorm(1000, mean = 5000, sd = 10000), 
-         daysAbsent = rnorm(1000, mean = 4, sd = 3))
+         daysAbsent = rnorm(1000, mean = 4, sd = 3), 
+         gender = sample(1:2, 1000, replace = TRUE, prob = c(.7, .3)))
 
 
 employment$daysAbsent[which(employment$daysAbsent < 0)] = 0
@@ -37,6 +39,9 @@ employment$education[which(employment$salary > quantile(employment$salary)[3])] 
 employment$education = ordered(employment$education, levels = 1:3, 
                                labels = c("high school", "bachelor", "post grad"))
 
-employment$daysEmployed[which(employment$daysEmployed > 330)] = 365
+employment$gender = factor(employment$gender, levels = 1:2, 
+                           labels = c("female", "male"))
 
 write.csv(employment, "inf/hw2Dat.csv", row.names = FALSE)
+
+save(employment, file = "inf/hw2Dat.RData")

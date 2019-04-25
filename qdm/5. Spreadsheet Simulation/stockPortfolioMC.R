2e610ded7407c1a908b03e3e6fa698aaafa19da8
Seth Berry <seth.berry@nd.edu>
2019-04-24 22:34:42 -0400

    stock portfolio

---
+++ qdm/5. Spreadsheet Simulation/stockPortfolioMC.R
@@ -0,0 +1,83 @@
+library(tidyquant)
+library(tidyverse)
+library(timetk)
+library(broom)
+library(highcharter)
+library(purrr)
+
+potentialSymbols = c("FB", "TSLA", "PYPL", "SNAP", "SIRI", "AMD", 
+                     "SPY","EFA", "IJS", "EEM","AGG")
+
+symbols = c("FB", "TSLA", "PYPL", "SNAP", "SIRI")
+
+prices = getSymbols(symbols, src = 'yahoo', 
+             from = "2012-12-31",
+             to = "2017-12-31",
+             auto.assign = TRUE, warnings = FALSE) %>% 
+  map(~Ad(get(.))) %>%
+  reduce(merge) %>%
+  `colnames<-` (symbols)
+
+w = c(0.25, 0.25, 0.20, 0.20, 0.10)
+
+asset_returns_long =  prices %>% 
+  to.monthly(indexAt = "lastof", OHLC = FALSE) %>% 
+  tk_tbl(preserve_index = TRUE, rename_index = "date") %>%
+  gather(asset, returns, -date) %>% 
+  group_by(asset) %>%  
+  mutate(returns = (log(returns) - log(lag(returns)))) %>% 
+  na.omit()
+
+portfolio_returns_tq_rebalanced_monthly = asset_returns_long %>%
+  tq_portfolio(assets_col = asset, 
+               returns_col = returns,
+               weights = w,
+               col_rename = "returns",
+               rebalance_on = "months")
+
+mean_port_return = mean(portfolio_returns_tq_rebalanced_monthly$returns)
+
+stddev_port_return = sd(portfolio_returns_tq_rebalanced_monthly$returns)
+
+
+simulation_accum_1 = function(init_value, N, mean, stdev) {
+  tibble(c(init_value, 1 + rnorm(N, mean, stdev))) %>% 
+    `colnames<-`("returns") %>%
+    mutate(growth = accumulate(returns, function(x, y) x * y)) %>% 
+    select(growth)
+}
+
+sims = 51
+
+starts = rep(1, sims) %>%
+  set_names(paste("sim", 1:sims, sep = ""))
+
+monte_carlo_sim_51 = map_dfc(starts, simulation_accum_1, 
+          N = 120, mean = mean_port_return, 
+          stdev = stddev_port_return)
+
+monte_carlo_sim_51 = monte_carlo_sim_51 %>% 
+  mutate(month = seq(1:nrow(.))) %>% 
+  select(month, everything()) %>% 
+  `colnames<-`(c("month", names(starts))) %>% 
+  mutate_all(funs(round(., 2))) 
+
+reruns = 51
+
+monte_carlo_rerun_51 = rerun(.n = reruns, 
+                             simulation_accum_1(1, 120, mean_port_return, 
+                                                stddev_port_return)) %>%
+  simplify_all() %>% 
+  `names<-`(paste("sim", 1:reruns, sep = " ")) %>%
+  as_tibble() %>% 
+  mutate(month = seq(1:nrow(.))) %>% 
+  select(month, everything())
+
+monte_carlo_sim_51 %>% 
+  gather(sim, growth, -month) %>% 
+  group_by(sim) %>% 
+  ggplot(aes(x = month, y = growth, color = sim)) + 
+  geom_line(alpha = .5, size = 1) + 
+  theme_minimal() +
+  theme(legend.position = "none")
+  

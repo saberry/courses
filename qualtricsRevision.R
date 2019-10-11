9440be26af5d314d54831f9c966ba4d324039abc
Seth Berry <seth.berry@nd.edu>
2019-10-11 14:31:20 -0400

    qualtrics revision

--- qualtricsRevision.R
+++ qualtricsRevision.R
@@ -1,56 +1,73 @@
 library(httr)
 
-username = "sberry5@nd.edu#nd"
+# username = "sberry5@nd.edu#nd"
 
 surveyID = "SV_9Fjwrg4TKCSg125"
 
 apiToken = "DSfaY34hc6jFdTOHiEns1sFf4IOtfuJbB15O0Xz2"
 
-fileFormat = "csv"
-
 dataCenter = 'ca1'
 
-# Setting static parameters
+getQualtricsData <- function(apiToken, dataCenter, surveyID) {
+   
+   baseUrl <- paste("https://", 
+                    dataCenter, 
+                    ".qualtrics.com/API/v3/surveys/",
+                    surveyID,
+                    "/export-responses", 
+                    sep = "")
+   
+   requestHeaders <- c("Content-Type" = "application/json", 
+                       "X-API-TOKEN" = apiToken)
+   
+   startSurveyDownload = POST(url = baseUrl, 
+                              body = list("format" = "json", 
+                                          "compress" = "false"),
+                              add_headers(.headers = requestHeaders), 
+                              encode = "json")
+   
+   requestID <- content(startSurveyDownload)$`result`$`progressId`
+   
+   progressLink <- paste(baseUrl, "/", requestID,
+                         sep = "")
+   
+   # The following gets reran:
+   
+   fileIDFunction <- function() {
+      progressComplete <- GET(url = progressLink,
+                              add_headers(.headers = requestHeaders))
+      
+      
+      fileID <- content(progressComplete)$`result`$`fileId`
+      
+      return(fileID)
+   }
+   
+   fileID <- fileIDFunction()
+   
+   
+   while(is.null(fileID)) {
+      fileID <- fileIDFunction()
+   }
 
-baseUrl = paste("https://", 
-                dataCenter, 
-                ".qualtrics.com/API/v3/surveys/",
-                surveyID,
-                "/export-responses", 
-                sep = "")
+   downloadLink <- paste(baseUrl, "/",
+                         fileID, "/file",
+                         sep = "")
+   
+   downloadData <- GET(url = downloadLink,
+                       add_headers(.headers = requestHeaders), progress())
+   
+   out <- jsonlite::fromJSON(content(downloadData, as = "text"), flatten = TRUE)$responses
+   
+   keepVariables <- grep("responseID|^values", names(out), value = TRUE)
+   
+   out <- out[, keepVariables]
+   
+   names(out) <- gsub("values\\.", "", names(out))
+   
+   return(out)
+}
 
-getSurveyData = POST(url = baseUrl, 
-                     body = list("format" = "csv", 
-                                 "compress" = "false"),
-                     add_headers(.headers = c("Content-Type" = "application/json", 
-                                              "X-API-TOKEN" = apiToken)), 
-                     encode = "json")
+debugonce(getQualtricsData)
 
-requestID = content(getSurveyData)$`result`$`progressId`
-
-progressMaker = paste("https://", 
-                     dataCenter, 
-                     ".qualtrics.com/API/v3/surveys/",
-                     surveyID,
-                     "/export-responses/",
-                     requestID,
-                     sep = "")
-
-progressComplete = GET(url = progressMaker,
-     add_headers(.headers = c("Content-Type" = "application/json", 
-                              "X-API-TOKEN" = apiToken)))
-
-
-fileID = content(progressComplete)$`result`$`fileId`
-
-dataLink = paste("https://", 
-      dataCenter, 
-      ".qualtrics.com/API/v3/surveys/",
-      surveyID,
-      "/export-responses/",
-      fileID, "/file",
-      sep = "")
-
-downloadData = GET(url = dataLink,
-                       add_headers(.headers = c("Content-Type" = "application/json", 
-                                                "X-API-TOKEN" = apiToken)))
+dataTest = getQualtricsData(apiToken, dataCenter, surveyID)

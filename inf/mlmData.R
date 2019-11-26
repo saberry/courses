263f675d5f2120ab9162adad12d049c0cf641b0b
Seth Berry <seth.berry@nd.edu>
2019-11-25 21:45:00 -0500

    mlm updates

--- inf/mlmData.R
+++ inf/mlmData.R
@@ -3,37 +3,52 @@ library(lme4)
 library(simstudy)
 
 gen.school <- defData(varname = "s0", dist = "normal", formula = 0, variance = 3, 
-                      id = "idSchool")
+                      id = "idBuilding")
 
-gen.school <- defData(gen.school, varname = "nClasses", dist = "noZeroPoisson", 
+gen.school <- defData(gen.school, varname = "nDepartments", dist = "noZeroPoisson", 
                       formula = 3)
 
 dtSchool <- genData(8, gen.school)
 
-dtSchool <- trtAssign(dtSchool, n = 2)
+# dtSchool <- trtAssign(dtSchool, n = 2)
 
 gen.class <- defDataAdd(varname = "c0", dist = "normal", formula = 0, variance = 2)
 
-gen.class <- defDataAdd(gen.class, varname = "nStudents", dist = "noZeroPoisson", 
+gen.class <- defDataAdd(gen.class, varname = "nEmployees", dist = "noZeroPoisson", 
                         formula = 20)
 
-dtClass <- genCluster(dtSchool, "idSchool", numIndsVar = "nClasses", level1ID = "idClass")
+dtClass <- genCluster(dtSchool, "idBuilding", numIndsVar = "nDepartments", level1ID = "idDepartment")
 
 dtClass <- addColumns(gen.class, dtClass)
 
-gen.student <- defDataAdd(varname = "Male", dist = "binary", 
+gen.student <- defDataAdd(varname = "Manager", dist = "binary", 
                           formula = 0.5)
+
 gen.student <- defDataAdd(gen.student, varname = "age", dist = "uniform", 
-                          formula = "9.5; 10.5")
-gen.student <- defDataAdd(gen.student, varname = "test", dist = "normal", 
-                          formula = "50 - 5*Male + s0 + c0 + 8 * trtGrp", variance = 2)
-dtStudent <- genCluster(dtClass, cLevelVar = "idClass", numIndsVar = "nStudents", 
-                        level1ID = "idChild")
+                          formula = "19.5; 72")
+
+gen.student <- defDataAdd(gen.student, varname = "performanceScore", dist = "normal", 
+                          formula = "50 - 5*Manager + s0 + c0 + 8  - age*.35", variance = 2)
+
+dtStudent <- genCluster(dtClass, cLevelVar = "idDepartment", numIndsVar = "nEmployees", 
+                        level1ID = "idEmployee")
 
 dtStudent <- addColumns(gen.student, dtStudent)
 
-summary(lm(test ~ trtGrp + Male, data = dtStudent))
+dtStudent <- dplyr::select(dtStudent, -s0, -c0)
 
-riMod = lmer(test ~ trtGrp + Male + (1|idSchool/idClass), data = dtStudent)
+write.csv(dtStudent, "inf/performanceReviewData.csv", row.names = FALSE)
+
+summary(lm(performanceScore ~ trtGrp + Manager + age, data = dtStudent))
+
+intMod <- lmer(performanceScore ~ trtGrp + 1 + (1|idDepartment), data = dtStudent)
+
+summary(intMod)
+
+riMod = lmer(performanceScore ~ trtGrp + Manager + (1|idDepartment), data = dtStudent)
+
+randomEffects <- lmer(performanceScore ~ trtGrp + Manager + (age|idBuilding), data = dtStudent)
+
+nestedMod = lmer(performanceScore ~ trtGrp + Manager + (1|idBuilding/idDepartment), data = dtStudent)
 
 summary(riMod)
